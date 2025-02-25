3a6da416b20fa3136bde18a9a5dbe61615298716
Viktor Egorov <netfantom.spb@gmail.com>
2025-02-25 22:58:37 +0300

    Updates

--- src/version.ts
+++ src/version.ts
@@ -1,4 +1,4 @@
 export const APP_NAME = 'nestjs-typeorm-pino-swagger-boilerplate',
-APP_VERSION = '1.9.0',
-APP_BUILD_DATE = '2025-02-20T08:09:33.157Z',
+APP_VERSION = '1.10.0',
+APP_BUILD_DATE = '2025-02-21T12:49:50.941Z',
 APP_COMMIT_HASH = '';
