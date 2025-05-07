14da0b72142d29a4f52487bf0a5e2ece98ec8a05
Viktor Egorov <netfantom.spb@gmail.com>
2025-05-07 16:56:49 +0300

    Update configureLoggingTransport helper

--- src/boilerplate/helpers/configuration/configure-logging.helper.ts
+++ src/boilerplate/helpers/configuration/configure-logging.helper.ts
@@ -4,8 +4,8 @@
  * @version 1.13
  * @summary Returns Pino transport configuration based on environment va
  */
-import { LogLevelEnum } from '@/boilerplate/types/enums/log-level.enum';
-import { LogTypeEnum } from '@/boilerplate/types/enums/log-type.enum';
+import { LogLevelEnum } from '@/boilerplate/common/enums/log-level.enum';
+import { LogTypeEnum } from '@/boilerplate/common/enums/log-type.enum';
 import { ConfigService } from '@nestjs/config';
 import { Params } from 'nestjs-pino';
 import { v4 as uuidv4 } from 'uuid';
